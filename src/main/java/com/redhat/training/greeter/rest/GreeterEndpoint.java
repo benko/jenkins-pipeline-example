b15fa25500e0a7d552e425e2583cfce1a25dcfce
Grega Bremec <gregab@p0f.net>
2025-04-10 21:20:22 +0300

    add english endpoint with name path param

--- src/main/java/com/redhat/training/greeter/rest/GreeterEndpoint.java
+++ src/main/java/com/redhat/training/greeter/rest/GreeterEndpoint.java
@@ -4,6 +4,7 @@ import com.redhat.training.greeter.service.EnglishGreeter;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
 @Path("/")
@@ -12,5 +13,10 @@ public class GreeterEndpoint {
     @GET
     public String greet() {
         return new EnglishGreeter().greet("stranger");
+    }
+    @GET
+    @Path("/{name}")
+    public String greet(@PathParam("name") String name) {
+        return new EnglishGreeter().greet(name);
     }
 }
